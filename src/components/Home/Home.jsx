0b52b7dda3b224d00ff384a11da3906909bc29b5
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-04-17 23:23:27 +0200

    framer

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,18 +1,35 @@
 import React from 'react'
 import Services from '../Services/Services'
+import { motion } from 'framer-motion'
 
 export default function Home() {
     return <>
 
         <div className='h-[25px] '></div>
         <div id='home' className="ccontainer text-center h-screen  flex items-center justify-center flex-col mx-auto ">
-            <h2 className='text-[20px] font-medium dark:text-white  text-gray-500'>Hello, I'm Bassant Ali and I'm a </h2>
-            <h2 className='lg:text-7xl text-3xl font-extrabold font-[poppins] mb-12 mt-5 lg:w-7/12 mx-auto dark:text-orange'>UI/UX Designer based in Egypt</h2>
-            <h2 className='lg:w-6/12 mx-auto text-[18px] font-medium dark:text-white  text-gray-500'>I love designing websites and user interfaces, doing freelance work and focusing on designing digital products that are easy on users and customers.</h2>
-            <div className='flex items-center gap-3 lg:gap-10 mt-14'>
+            <motion.h2
+                initial={{ opacity: 0, x: -100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: .4 }}
+                className='text-[20px] font-medium dark:text-white  text-gray-500'>Hello, I'm Bassant Ali and I'm a </motion.h2>
+            <motion.h2
+                initial={{ opacity: 0, x: 100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: .4, delay: .4 }}
+                className='lg:text-7xl text-3xl font-extrabold font-[poppins] mb-12 mt-5 lg:w-7/12 mx-auto dark:text-orange'>UI/UX Designer based in Egypt</motion.h2>
+            <motion.h2
+                initial={{ opacity: 0, x: -100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{ duration: .4, delay: .6 }}
+                className='lg:w-6/12 mx-auto text-[18px] font-medium dark:text-white  text-gray-500'>I love designing websites and user interfaces, doing freelance work and focusing on designing digital products that are easy on users and customers.</motion.h2>
+            <motion.div
+                initial={{ opacity: 0, scale:0.8 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                transition={{ duration: .4 , delay:.7}}
+                className='flex items-center gap-3 lg:gap-10 mt-14'>
                 <a className=' p-4 lg:px-6 bg-orange hover:bg-transparent hover:text-orange border border-transparent hover:border-orange duration-300 ease-in-out font-medium font-[poppins] text-white rounded-4xl' href="#projects">Check my projects</a>
                 <a className=' p-4 lg:px-6 text-orange border border-orange rounded-4xl font-normal hover:bg-orange hover:text-white duration-300 font-[poppins] flex items-center gap-2' href="mailto:bassantalielfaqy@gmail.com"> <i class='bx bx-envelope' ></i>Email me</a>
-            </div>
+            </motion.div>
         </div>
 
 
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -3,6 +3,7 @@ import img from '../../assets/bg.png'
 import img1 from '../../assets/design-service.c8c074f8.svg'
 import img2 from '../../assets/graphic-design.81ae9670.svg'
 import img3 from '../../assets/ui-ux-design.7f8252c2.svg'
+import { motion } from 'framer-motion'
 
 
 
@@ -16,24 +17,40 @@ export default function Services() {
             {/* <div className='h-[95px] bg-black'></div> */}
 
             <div id='services' className={` ccontainer  bg-black text-5xl bg-cover bg-no-repeat bg-center min-h-screen  flex flex-col items-center justify-center`} style={{ backgroundImage: `url(${img})` }}>
-                <h2 className='lg:text-6xl text-3xl font-bold font-[poppins] text-white text-center'>What can I do for you</h2>
+                <motion.h2
+                    initial={{ opacity: 0, x: 200 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ duration: .4 }}
+                    className='lg:text-6xl text-3xl font-bold font-[poppins] text-white text-center'>What can I do for you</motion.h2>
                 <div className='flex flex-col lg:flex-row gap-4 items-center justify-center mt-12'>
 
-                    <div className='p-8 lg:w-4/12 bg-white dark:bg-fd rounded-4xl text-center'>
+                    <motion.div
+                        initial={{ opacity: 0, scale: .8 }}
+                        whileInView={{ opacity: 1, scale: 1 }}
+                        transition={{duration:.4 ,delay:.5}}
+                        className='p-8 lg:w-4/12 bg-white dark:bg-fd rounded-4xl text-center'>
                         <div className=' flex items-center  justify-center h-32  w-32 mx-auto mb-3 bg-orange rounded-full'><img src={img3} alt="" /></div>
                         <h3 className=' font-bold  text-3xl dark:text-white'>UI/UX Design</h3>
                         <h3 className='text-2xl text-gray-600 dark:text-gray-400 mt-2'>Designing interfaces that are effective and easy to use by users and customers</h3>
-                    </div>
-                    <div className='p-8 lg:w-4/12 bg-white dark:bg-fd rounded-4xl text-center'>
-                        <div className=' flex items-center  justify-center h-32  w-32 mx-auto mb-3 bg-orange rounded-full'><img src={img3} alt="" /></div>
+                    </motion.div>
+                    <motion.div
+                        initial={{ opacity: 0, scale: .8 }}
+                        whileInView={{ opacity: 1, scale: 1 }}
+                        transition={{duration:.4 ,delay:.6}}
+                        className='p-8 lg:w-4/12 bg-white dark:bg-fd rounded-4xl text-center'>
+                        <div className=' flex items-center  justify-center h-32  w-32 mx-auto mb-3 bg-orange rounded-full'><img src={img2} alt="" /></div>
                         <h3 className=' font-bold  text-3xl dark:text-white'>UI/UX Design</h3>
                         <h3 className='text-2xl text-gray-600 dark:text-gray-400 mt-2'>Designing interfaces that are effective and easy to use by users and customers</h3>
-                    </div>
-                    <div className='p-8 lg:w-4/12 bg-white dark:bg-fd rounded-4xl text-center'>
-                        <div className=' flex items-center  justify-center h-32  w-32 mx-auto mb-3 bg-orange rounded-full'><img src={img3} alt="" /></div>
+                    </motion.div>
+                    <motion.div
+                        initial={{ opacity: 0, scale: .8 }}
+                        whileInView={{ opacity: 1, scale: 1 }}
+                        transition={{duration:.4 ,delay:.7}}
+                        className='p-8 lg:w-4/12 bg-white dark:bg-fd rounded-4xl text-center'>
+                        <div className=' flex items-center  justify-center h-32  w-32 mx-auto mb-3 bg-orange rounded-full'><img src={img1} alt="" /></div>
                         <h3 className=' font-bold  text-3xl dark:text-white'>UI/UX Design</h3>
                         <h3 className='text-2xl text-gray-600 dark:text-gray-400 mt-2'>Designing interfaces that are effective and easy to use by users and customers</h3>
-                    </div>
+                    </motion.div>
 
 
                 </div>
